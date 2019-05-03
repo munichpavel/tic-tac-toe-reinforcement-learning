8cd58c1b2362a902761a13a5ac998058ab4b5e24
munichpavel <munichpavel@gmail.com>
2019-05-03 04:56:08 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,61 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+# io.open is needed for projects that support Python 2.7
+# It ensures open() defaults to text mode with universal newlines,
+# and accepts an argument to specify the text encoding
+# Python 3 only projects can skip this import
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='tic-tac-toe-reinforcement-learning',  # Required
+    version='0.0.1',  # Required
+    description='Reinforcement learning agents for tic-tac-toe',  # Optional
+
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown',  # Optional (see note above)
+    url='https://github.com/munichpavel/tic-tac-toe-reinforcement-learning',
+
+    author='eidonfiloi + munichpavel',  # Optional
+    author_email='munichpavel@gmail.com',  # Optional
+
+    classifiers=[  # Optional
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+
+    packages=find_packages(include=['src'], exclude=['contrib', 'docs', 'tests']),  # Required
+
+    python_requires='>=3.6',
+
+    install_requires=['matplotlib', 'pandas', 'pytest'],  # Optional
+
+)
